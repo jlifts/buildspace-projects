a50d8b0ee2bbe9764b46463c0185b766255e7f88
Josh Burgess <burgessj247@gmail.com>
2022-01-06 21:11:13 -0500

    Finishing touches on the basics of the front-end

---
+++ epic-game/client/src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x976ec63a2f49d2d6Eb4b45e2c8CE9bFBEC4CcFF7';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
